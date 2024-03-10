779cc79f69e1d0942517d546b3bc8e1327a3775b
Adam Heinermann <aheinerm@gmail.com>
2024-03-10 03:20:43 -0700

    Clean up n64 algo slightly

--- BOLT/bolt-extract/n64.cpp
+++ BOLT/bolt-extract/n64.cpp
@@ -1,4 +1,5 @@
 #include "bolt.h"
+#include "util.h"
 
 using namespace BOLT;
 
@@ -45,21 +46,15 @@ void bolt_reader_t::decompress_n64(std::uint32_t offset, std::uint32_t expected_
         break;
       }
 
-      std::int32_t target_offset = std::int32_t(result.size()) - 1 - ((ext_offset << 4) | (bytevalue & 0xF));
+      std::uint32_t rel_offset = ((ext_offset << 4) | (bytevalue & 0xF)) + 1;
       std::uint32_t run_length = ((ext_run << 3) | (bytevalue >> 4)) + op_count + 1;
 
-      if (target_offset < 0) {
-        err_msg("negative value for lookbehind", bytevalue);
-        break;
-      }
-      if (result.size() <= target_offset) {
+      if (result.size() < rel_offset) {
         err_msg("lookbehind too far", bytevalue);
         break;
       }
 
-      for (unsigned i = 0; i < run_length; ++i) {
-        result.push_back(result[target_offset + i]);
-      }
+      reinsert_self(result, rel_offset, run_length);
       op_count = ext_offset = ext_run = 0;
     }
   }
